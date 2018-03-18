9edd571d241f679d3b0b15e3bc1c44b8e976c5bb
sunaliseth <sunaliseth01@gmail.com>
2018-03-18 18:14:52 +0000

    Express Router; setup route file

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import config from './config';
-import express from 'express';
+import router from './api';
 
+import express from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
   res.send('Hello Express');
 });
-
+//express middleware
+server.use('/api', router);
 server.use(express.static('public'));
 
 server.listen(config.port, () => {
