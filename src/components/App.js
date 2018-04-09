0ae2caa135d126ed54407705582e47e332be0e0b
sunaliseth <sunaliseth01@gmail.com>
2018-04-09 17:09:57 +0100

    Assign state to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div className= "App">
-      <Header message= 'Naming Contests' />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = {
+    test: 47
+  };
+  render(){
+    return (
+      <div>
+        <div className= "App">
+          <Header message= 'Naming Contests' />
+        </div>
+        <div>
+          {this.state.test}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
