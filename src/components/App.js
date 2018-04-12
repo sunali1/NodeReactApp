d935319a2e993ee1bb15fdd5fa2e6dbc67a8696d
sunaliseth <sunaliseth01@gmail.com>
2018-04-12 12:45:07 +0100

    Fetched data from the remote api and served it through App component state. Used axios for the ajax request

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
+
 
 
 class App extends React.Component {
@@ -9,9 +11,16 @@ class App extends React.Component {
     contests: []
   };
   componentDidMount() {
-    this.setState ({
-      contests: data.contests
-    });
+    axios.get('/api/contests')
+      .then(response => {
+        console.log(response);
+        this.setState ({
+          contests: response.data.contests
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
   componentWillUnmount() {
 
